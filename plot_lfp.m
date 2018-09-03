1e2dc3109699af12bf61db502744f369edd40085
sahar <sahar.rajabi76@gmail.com>
2018-09-03 11:19:58 +0430

    plot lfp signal documentation added.

--- plot_lfp.m
+++ plot_lfp.m
@@ -1,3 +1,21 @@
+%% LFP Signal
+% This function will calculate and plot the LFP signal, which can be obtained
+% from the continous data in the .NSx files recieved from blackrock system.
+%
+% The inputs should be as follows:
+%
+% * continuous_data: The contineous data reletive to the experiment's trials.
+% * continuous_data: The timestamps corresponding to the previous argument.
+% * feature: This should contain the whole possible values of the stimulus variable
+% that is mined in the experiment.
+% * feature_name : This argument, is stand for the name of that stimulus variable
+% in trials data.
+% * Trials: Clear, the trials exist in the experiment.
+% * x_low_lim: The low limit of the x axis.
+% * x_high_lim: The high limit of the x axis.
+% * x_axis_partitions: The numbers we want to display on the x axis.
+%
+
 function plot_lfp ( ...
             continuous_data, ...
             continuous_data_time, ...
